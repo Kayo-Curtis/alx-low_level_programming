3b03567b911b85e71cf90d3020a0797dc39dffe0
Kayo-Curtis <kayocurtiswilson@gmail.com>
2023-02-23 05:56:48 +0300

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,19 +7,17 @@
  */
 void print_alphabet_x10(void)
 {
-int n;
-int count;
+	int n, co;
 
+	co = 0;
 
-count = 0;
-while (count < 10)
-{
-for (n = 'a' ; n <= 'z'; n++)
-{
-_putchar(n);
-}
-
-count++;
-_putchar('\n');
-}
+	while (co < 10)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		co++;
+		_putchar('\n');
+	}
 }
