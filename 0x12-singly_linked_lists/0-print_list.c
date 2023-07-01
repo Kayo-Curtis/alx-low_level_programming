6aa220441bb7d5ef82cb0910f7c0362fab27b0d2
Kayo-Curtis <kayocurtiswilson@gmail.com>
2023-07-01 15:55:52 +0300

    0-print_list.c update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,9 +15,13 @@ size_t print_list(const list_t *h)
 	current = h;
 	while (current != NULL)
 	{
-		printf("[%d] %s\n", current->len, current->str);
-		current = current->next;
-		c++;
+		if (current->str == NULL)
+            printf("[0] (nil)\n");
+        else
+            printf("[%d] %s\n", current->len, current->str);
+
+        current = current->next;
+        c++;
 	}
 
 	return (c);
