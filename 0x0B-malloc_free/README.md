0f2ae31ffe84801cdda2bb6fe2238d071992a8fd
Kayo-Curtis <kayocurtiswilson@gmail.com>
2023-07-04 22:54:44 +0300

    all malloc and free tasks tested

---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,5 @@
+0x0B. C - malloc, free
+
+malloc-Memory allocation.
+
+malloc(size_t size)
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,8 @@
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+void free_grid(int **grid, int height);
+char *str_concat(char *s1, char *s2);
+int _putchar(char c);
+int **alloc_grid(int width, int height);
+char *argstostr(int ac, char **av);  
+char **strtow(char *str);
